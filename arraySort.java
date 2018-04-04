4f616c70274740b670b4d689d9c6da9191e6186c
daniel1982007 <daniel1982007@gmail.com>
2018-04-04 13:51:20 +0300

    create an array and be sorted

---
+++ arraySort.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class arraySort{
+
+  public static void main(String[] args){
+    
+    int[] a = {2,9,12,6,89,81,62,5,3,11}; 
+    Arrays.sort(a);
+    String myArray = Arrays.toString(a);
+    System.out.println(myArray);
+  
+  }
+
+ }
