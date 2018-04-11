3194b77fb308f5377915fa45628b72fe97271707
daniel1982007 <daniel1982007@gmail.com>
2018-04-11 17:25:19 +0300

    BySwitch

---
+++ IntegerCountingBySwitch.java
@@ -0,0 +1,32 @@
+public class IntegerCountingBySwitch{
+    public static void main(String[] args){
+        int a = 10;
+        switch(a){
+          case 10:
+              System.out.println(10);
+          case 11:
+              System.out.println(11);
+          case 12:
+              System.out.println(12);
+          case 13:
+              System.out.println(13);
+          case 14:
+              System.out.println(14);
+          case 15:
+              System.out.println(15);
+          case 16:
+              System.out.println(16);
+          case 17:
+              System.out.println(17);
+          case 18:
+              System.out.println(18);
+          case 19:
+              System.out.println(19);
+          case 20:
+              System.out.println(20);
+
+
+}
+
+}
+}
