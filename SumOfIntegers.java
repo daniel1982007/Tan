1aa6c49d3c326c4756c6cfe62502f84fbc24590e
daniel1982007 <daniel1982007@gmail.com>
2018-04-11 17:20:22 +0300

    SumOfIntegers

---
+++ SumOfIntegers.java
@@ -0,0 +1,13 @@
+public class SumOfIntegers{
+    public static void main(String[] args){
+	int[] a = new int[10];
+	
+	int b = 0;
+	
+	for(int i : a){
+	b = b+i;
+	   
+}
+	System.out.print(b);
+}
+}
