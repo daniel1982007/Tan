e85409835b369f63a09a6bb6dd2e8b62ddec0741
daniel1982007 <daniel1982007@gmail.com>
2018-04-11 17:26:26 +0300

    ByWhile

---
+++ IntegerCountingByWhile.java
@@ -0,0 +1,10 @@
+public class IntegerCountingByWhile{
+    public static void main(String[] args){
+        int a = 10;
+        while(a>=10 && a<=20){
+        
+        System.out.println(a);
+        a++;
+}
+}
+}
